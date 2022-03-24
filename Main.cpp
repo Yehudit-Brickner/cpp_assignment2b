e005bbbf4e0ec18e9719792af160aa45c158f673
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-24 10:39:55 +0200

    added a small main, and changed the illegal symbs in Notebook

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+#include <random>
+// #include<cstdlib>
+// #include <string>
+// #include <vector>
+#include "Notebook.hpp"
+using namespace std;
+using ariel::Direction;
+
+
+
+int main(){
+    
+   ariel::Notebook notebook1;
+   notebook1.write(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Horizontal,"hello world");
+   notebook1.read(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Horizontal,11);
+   return 0;
+}
